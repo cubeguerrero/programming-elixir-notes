b96d244ebef251fed537dd334f9f9a78f9c82a03
Cuthbert Guerrero <cuthbert.guerrero@gmail.com>
2017-04-06 21:37:00 +0800

    minor fixes for list-and-recursion-5

--- chapter-10/list-and-recursion-5/my_list.exs
+++ chapter-10/list-and-recursion-5/my_list.exs
@@ -17,12 +17,14 @@ defmodule MyList do
   def split(list, count) when count < 0, do: split(list, _count(list) + count)
   def split(list, count), do: _split(list, count, [])
   defp _split(list, 0, result), do: {result, list}
+  defp _split([], _, result), do: _split([], 0, result)
   defp _split([head | tail], count, result), do: _split(tail, count - 1, result ++ [head])
 
   # Wrong when passed negative values.
   def take(list, count) when count < 0, do: take(reverse(list), count * -1)
   def take(list, count), do: _take(list, count, [])
   defp _take(_list, 0, result), do: result
+  defp _take([], _, result), do: _take([], 0, result)
   defp _take([head | tail], count, result), do: _take(tail, count - 1, result ++ [head])
 
   defp _count([]), do: 0
@@ -39,6 +41,8 @@ IO.puts MyList.all?([1, 2, 3, 4, 5], &(&1 < 6)) #=> true
 IO.inspect MyList.each([1, 2, 3, 4, 5], &(&1 * &1)) #=> [1, 4, 9, 16, 25]
 IO.inspect MyList.filter([1, 2, 3, 4, 5], &Integer.is_even/1) #=> [2, 4]
 IO.inspect MyList.split([1, 2, 3, 4, 5], 3)
+IO.inspect MyList.split([1, 2, 3, 4, 5], 6)
 IO.inspect MyList.split([1, 2, 3, 4, 5], -3)
 IO.inspect MyList.take([1, 2, 3, 4, 5], 3)
+IO.inspect MyList.take([1, 2, 3, 4, 5], 6)
 IO.inspect MyList.take([1, 2, 3, 4, 5], -3)
